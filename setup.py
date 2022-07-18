6b5dc07d89bee2e690fc9d2e8cb03113b9285e4f
Joris Gillis <joris.gillis42@gmail.com>
2022-07-18 14:36:04 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import glob
+import os
+
+version = "0.1.1"
+
+setup(
+    name='dirac-mpc',
+    version=version,
+    author="MECO-Group",
+    author_email="joris.gillis@kuleuven.be",
+    description="DIRAC MPC",
+    license='LICENSE',
+    url='https://gitlab.kuleuven.be/meco/projects/sbo_dirac/dirac_mpc',
+    packages=find_packages(exclude=['tests', 'examples']),
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'casadi>=3.5,<4.0',
+    ],
+    download_url='https://gitlab.kuleuven.be/meco/projects/sbo_dirac/dirac_mpc/-/archive/v%s/dirac-mpc-v%s.tar.gz' % (version, version)
+)
