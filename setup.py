fe2cc183abf66697a98656586ba55f63ac907fab
Joris Gillis <joris.gillis42@gmail.com>
2023-03-13 12:54:49 +0100

    bump rockit-meco

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'casadi>=3.5,<4.0',
         'pyyaml',
         'lxml',
-        'rockit-meco>=0.1.29',
+        'rockit-meco>=0.1.31',
     ],
     download_url='https://gitlab.kuleuven.be/meco/projects/sbo_dirac/impact/-/archive/v%s/impact-v%s.tar.gz' % (version, version)
 )
